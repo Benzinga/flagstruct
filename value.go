dc9b35fe549280f18ae0239c8f1615d54cf81202
John Chadwick <johnwchadwick@gmail.com>
2017-03-06 11:46:44 -0500

    Fix golint warnings.

--- value.go
+++ value.go
@@ -1,4 +1,5 @@
 // This is copied from Golang's flag library, because it is private :(
+
 package flagstruct
 
 import (
@@ -11,108 +12,134 @@ import (
 // BoolValue represents a boolean value.
 type BoolValue bool
 
+// Set implements the Value interface.
 func (b *BoolValue) Set(s string) error {
 	v, err := strconv.ParseBool(s)
 	*b = BoolValue(v)
 	return err
 }
 
+// Get implements the Value interface.
 func (b *BoolValue) Get() interface{} { return bool(*b) }
 
+// String implements the Value interface.
 func (b *BoolValue) String() string { return fmt.Sprintf("%v", *b) }
 
+// IsBoolFlag signals boolean flag behavior to Go's flag library.
 func (b *BoolValue) IsBoolFlag() bool { return true }
 
 // IntValue represents an integer value.
 type IntValue int
 
+// Set implements the Value interface.
 func (i *IntValue) Set(s string) error {
 	v, err := strconv.ParseInt(s, 0, 64)
 	*i = IntValue(v)
 	return err
 }
 
+// Get implements the Value interface.
 func (i *IntValue) Get() interface{} { return int(*i) }
 
+// String implements the Value interface.
 func (i *IntValue) String() string { return fmt.Sprintf("%v", *i) }
 
 // Int64Value represents a 64-bit integer value.
 type Int64Value int64
 
+// Set implements the Value interface.
 func (i *Int64Value) Set(s string) error {
 	v, err := strconv.ParseInt(s, 0, 64)
 	*i = Int64Value(v)
 	return err
 }
 
+// Get implements the Value interface.
 func (i *Int64Value) Get() interface{} { return int64(*i) }
 
+// String implements the Value interface.
 func (i *Int64Value) String() string { return fmt.Sprintf("%v", *i) }
 
 // UintValue represents an unsigned integer value.
 type UintValue uint
 
+// Set implements the Value interface.
 func (i *UintValue) Set(s string) error {
 	v, err := strconv.ParseUint(s, 0, 64)
 	*i = UintValue(v)
 	return err
 }
 
+// Get implements the Value interface.
 func (i *UintValue) Get() interface{} { return uint(*i) }
 
+// String implements the Value interface.
 func (i *UintValue) String() string { return fmt.Sprintf("%v", *i) }
 
-// Int64Value represents an unsigned 64-bit integer value.
+// Uint64Value represents an unsigned 64-bit integer value.
 type Uint64Value uint64
 
+// Set implements the Value interface.
 func (i *Uint64Value) Set(s string) error {
 	v, err := strconv.ParseUint(s, 0, 64)
 	*i = Uint64Value(v)
 	return err
 }
 
+// Get implements the Value interface.
 func (i *Uint64Value) Get() interface{} { return uint64(*i) }
 
+// String implements the Value interface.
 func (i *Uint64Value) String() string { return fmt.Sprintf("%v", *i) }
 
 // StringValue represents a string value.
 type StringValue string
 
+// Set implements the Value interface.
 func (s *StringValue) Set(val string) error {
 	*s = StringValue(val)
 	return nil
 }
 
+// Get implements the Value interface.
 func (s *StringValue) Get() interface{} { return string(*s) }
 
+// String implements the Value interface.
 func (s *StringValue) String() string { return fmt.Sprintf("%s", *s) }
 
 // Float64Value represents a 64-bit floating point value.
 type Float64Value float64
 
+// Set implements the Value interface.
 func (f *Float64Value) Set(s string) error {
 	v, err := strconv.ParseFloat(s, 64)
 	*f = Float64Value(v)
 	return err
 }
 
+// Get implements the Value interface.
 func (f *Float64Value) Get() interface{} { return float64(*f) }
 
+// String implements the Value interface.
 func (f *Float64Value) String() string { return fmt.Sprintf("%v", *f) }
 
 // DurationValue represents a duration of time.
 type DurationValue time.Duration
 
+// Set implements the Value interface.
 func (d *DurationValue) Set(s string) error {
 	v, err := time.ParseDuration(s)
 	*d = DurationValue(v)
 	return err
 }
 
+// Get implements the Value interface.
 func (d *DurationValue) Get() interface{} { return time.Duration(*d) }
 
+// String implements the Value interface.
 func (d *DurationValue) String() string { return (*time.Duration)(d).String() }
 
+// Value is an interface used for flag values.
 type Value interface {
 	String() string
 	Set(string) error
